6c01a9760c9fb9280f840e8293635a74f0b34e02
AngeluzFranco <20223TN093@utez.edu.mx>
2025-11-24 17:26:49 -0600

    Add: Cache en service worker para instalación en Android

--- sw.js
+++ sw.js
@@ -1,9 +1,28 @@
+const CACHE_NAME = "geo-pwa-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
 self.addEventListener("install", (event) => {
   console.log("SW: instalado");
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   console.log("SW: activado");
   return self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
